80b1d21f9f80b1195db04b83a46c94a837b33558
HEIZENNBERG <oussama171103@gmail.com>
2023-02-04 20:20:41 +0100

    init commit

---
+++ app/src/main/java/com/example/my_pfe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.my_pfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
